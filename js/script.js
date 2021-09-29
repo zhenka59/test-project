ee022d87a9bf24b6f9384756a1917c6d535892e4
zhenka59 <zhenka-a@yandex.ru>
2021-09-30 01:42:32 +0500

    fix

--- js/script.js
+++ js/script.js
@@ -1,48 +1,54 @@
-$('.sliderBox').slick({
-            infinite: true,
-            slidesToShow: 2,
-            // autoplay: true,
-            // autoplaySpeed: '3000',
-            arrows : true,
-            // dots: true,
-            slidesToScroll: 1,
-            responsive: [{
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    // arrows : false
-                }
-            }, ]
-        });
-
-
-
 function toggleAccordion() {
-    $('.accordion__name').on('click', function() {
-        let currentDrop = $(this).closest('.accordion__item').find('.accordion__drop');
-        $('.accordion__drop').not(currentDrop).slideUp();
-        $('.accordion__toggle').not($(this)).removeClass('active');
+    $( '.accordion__name' ).on( 'click', function () {
+        let currentDrop = $( this ).closest( '.accordion__item' ).find( '.accordion__drop' );
+        $( '.accordion__drop' ).not( currentDrop ).slideUp();
+        $( '.accordion__toggle' ).not( $( this ) ).removeClass( 'active' );
         currentDrop.slideToggle();
-        $(this).toggleClass('open');
+        $( this ).toggleClass( 'open' );
 
-    });
+    } );
 }
+var app = {
+    pageScroll: '',
+    lgWidth: 1200,
+    mdWidth: 992,
+    smWidth: 768,
+    resized: false,
+    iOS: function () {
+        return navigator.userAgent.match( /iPhone|iPad|iPod/i );
+    },
+    touchDevice: function () {
+        return navigator.userAgent.match( /iPhone|iPad|iPod|Android|BlackBerry|Opera Mini|IEMobile/i );
+    }
+};
+
+function isLgWidth() {
+    return $( window ).width() >= app.lgWidth;
+} // >= 1200
+function isMdWidth() {
+    return $( window ).width() >= app.mdWidth && $( window ).width() < app.lgWidth;
+} //  >= 992 && < 1200
+function isSmWidth() {
+    return $( window ).width() >= app.smWidth && $( window ).width() < app.mdWidth;
+} // >= 768 && < 992
+function isXsWidth() {
+    return $( window ).width() < app.smWidth;
+} // < 768
+function isIOS() {
+    return app.iOS();
+} // for iPhone iPad iPod
+function isTouch() {
+    return app.touchDevice();
+} // for touch device
+
 toggleAccordion();
-function swichTabs() {
-    let tabs = $('.tabs-astax'),
-        tab = $('.tab-astax'),
-        panel = $('.panel'),
-        id;
 
-    tab.on('click', (e) => {
-        let self = e.target;
-        tab.removeClass('active');
-        panel.removeClass('active');
-        $(self).addClass('active');
-        id = $(self).data('panel');
-        $(id).addClass('active');
-    });
+function footerButton() {
+    const btn = $( '.footer__button' );
+    if ( isXsWidth() ) {
+        btn.appendTo( '.footer__app' );
+    } else {
+        btn.appendTo( '.footer__action' );
+    }
 }
-
-swichTabs();
+footerButton();
